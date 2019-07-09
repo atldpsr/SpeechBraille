d934514b4f586aadc3e64075a3048511cf81493c
atl dpsr <atldpsr@gmail.com>
2019-07-09 07:49:28 +0000

    Adds project files

---
+++ Servo_control.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
+
+pwm = GPIO.PWM(7, 50)
+
+pwm.start(0)
+
+def setAngle(angle):
+	duty = angle/18 + 2
+	GPIO.output(7, True)
+	pwm.ChangeDutyCycle(duty)
+	sleep(1)
+	GPIO.output(7, False)
+	pwm.ChangeDutyCycle(0)
+
+setAngle(0)
+sleep(1)
+setAngle(90)
+sleep(1)
+setAngle(0)
+
+
+pwm.stop()
+GPIO.cleanup()
